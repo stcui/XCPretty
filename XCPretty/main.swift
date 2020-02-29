bc3a1f19b38bca9d3fef986916e4ce416d4aafa7
StevenChoi <stcui@me.com>
2020-02-29 11:43:40 +0800

    Initial Commit

---
+++ XCPretty/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  XCPretty
+//
+//  Created by Steven Choi on 2020/2/29.
+//  Copyright © 2020 Steven Choi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
